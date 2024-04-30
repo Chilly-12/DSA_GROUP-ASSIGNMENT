0ff3789a1e9d403c2997120a889bf68c8dd575a9
Chilly-12 <141146455+Chilly-12@users.noreply.github.com>
2024-04-30 17:23:58 +0200

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Star {
+    private Node centralNode;
+    private List<Node> peripheralNodes;
+
+    public Star() {
+        this.centralNode = new Node();
+        this.peripheralNodes = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Star star = new Star();
+   
+    }
+}
+
+class Node {
+    private List<Object> dataValues;
+
+    public Node() {
+        this.dataValues = new ArrayList<>();
+    }
+
+
+}
