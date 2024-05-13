4f00b5e63399e42f2ca825007b9e5c90784ace92
Amwaalwa0209 <141063617+Amwaalwa0209@users.noreply.github.com>
2024-05-13 14:58:03 +0200

    Update ClientNode.java

--- ClientNode.java
+++ ClientNode.java
@@ -1,3 +1,9 @@
+class ServerNode {
+    public void sendMessage(String senderId, String receiverId, String message) {
+        System.out.println("Message sent from " + senderId + " to " + receiverId + ": " + message);
+    }
+}
+
 class  ClientNode{
     private String clientId;
 
@@ -5,10 +11,20 @@ class  ClientNode{
 
         this.clientId = clientId;
     }
-    public void sendMessage(ServerNode server, String receiverId,String message){
+    public void send(ServerNode server, String receiverId,String message){
         server.sendMessage(clientId,receiverId,message);
     }
-    public void receiveMessage(String senderId,String message){
+    public void receive(String senderId,String message){
         System.out.println("Received message from" + senderId + ":" + message);
+    }
+
+    public static void main(String[] args) {
+
+        ClientNode client = new ClientNode("client1");
+        ServerNode server = new ServerNode();
+
+        client.send(server, "server", "Hello from client!");
+
+        client.receive("server", "Hello back from server!");
     }
 }
