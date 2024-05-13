f3ede5077fdd06709fa6f76154edd6468fce51b0
Tidex-07 <165654249+Tidex-07@users.noreply.github.com>
2024-05-13 15:56:23 +0200

    Update (i)ServerNode.java

--- (i)ServerNode.java
+++ (i)ServerNode.java
@@ -2,34 +2,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServerNode {
-    private List<PeripheralNode> peripheralNodes;
-    private String data1;
-    private String data2;
+    private List<ClientNode> clientNodes;
 
     public ServerNode() {
-        this.peripheralNodes = new ArrayList<>();
+        this.clientNodes = new ArrayList<>();
     }
 
-    // Method to add peripheral nodes to the server node
-    public void addPeripheralNode(PeripheralNode node) {
-        peripheralNodes.add(node);
+    // Method to add client nodes to the server node
+    public void addClientNode(ClientNode node) {
+        clientNodes.add(node);
     }
 
-    // Method to broker messages sent by client node objects
-    public void brokerMessage(String message) {
-        // Process and distribute the message to peripheral nodes
-        for (PeripheralNode node : peripheralNodes) {
+    // Method to broadcast a message to all connected client nodes
+    public void broadcastMessage(String message) {
+        for (ClientNode node : clientNodes) {
             node.receiveMessage(message);
         }
     }
 
-    // Setter method for data1
+    // Method to send a message to a specific client node
+    public void sendMessageToClient(ClientNode clientNode, String message) {
+        if (clientNodes.contains(clientNode)) {
+            clientNode.receiveMessage(message);
+        } else {
+            System.out.println("Client node not found in the network.");
+        }
+    }
+
+    // Setter methods for data1 and data2
+    // These setters are optional and can be used to set data specific to the server node
+    // You can remove them if not needed
     public void setData1(String data1) {
+        // Implement data validation or other logic if needed
         this.data1 = data1;
     }
 
-    // Setter method for data2
     public void setData2(String data2) {
+        // Implement data validation or other logic if needed
         this.data2 = data2;
     }
 }
